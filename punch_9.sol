0bab81001152e95d04a95d31e6fba17473da107a
abib03 <abib.ajibola@gmail.com>
2022-05-25 19:27:42 -0500

    fixed NFT URL issue and uploaded abi.json file for front end minting

--- punch_9.sol
+++ punch_9.sol
@@ -1,17 +1,20 @@
 pragma solidity ^0.5.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
-// tokenURI= "https://github.com/mitchjr9/Project-3/blob/main/images/Punch%209%20Poster.png";
+
 contract ArtToken is ERC721Full {
+    string tokenURI = "https://github.com/mitchjr9/Project-3/blob/main/images/Punch%209%20Poster.png";
+
     constructor() public ERC721Full("Punch9", "PUNCH9") {}
-    
-    function registerArtwork(address owner, string memory tokenURI)
+
+    function registerArtwork(address owner)
         public
         returns (uint256)
     {
-        uint256 tokenId = totalSupply();
-        _mint(owner, tokenId);
-        _setTokenURI(tokenId, tokenURI);
+        
+        uint256 tokenId = totalSupply(); // 1. create unique token ID 
+        _mint(owner, tokenId);           // 2. mint PUNCH9 token for token ID
+        _setTokenURI(tokenId, tokenURI); // 3. associate in the process of mining the token ID with URI
 
-        return tokenId;
+        return tokenId;                  // 4. return token ID value
     }
 }
